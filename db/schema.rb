14c3d883ee109560eeb7b3b1dfb8da2f52b78699
RajSannidhi <rajesh.v.sannidhi@gmail.com>
2017-06-21 17:52:25 -0400

    Deleted database and created new database

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170621172728) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-    t.float    "latitutde"
+    t.float    "latitude"
     t.float    "longitude"
     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
